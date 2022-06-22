1b62d9987e85eb0da5c1ed42ae476dfdb6517d82
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-06-22 14:19:58 +0800

    add React router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Home from "./components/Home";
 import Backdrop from "./components/Backdrop";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
+import NoMatch from "./components/NoMatch";
 
 const darkTheme = createTheme({
   palette: {
@@ -14,8 +17,11 @@ function App() {
     <div>
     {/* <ThemeProvider theme={darkTheme}> */}
         <Backdrop />
-        <Home />
-          yolo fefew
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
     {/* </ThemeProvider> */}
     </div>
   );
